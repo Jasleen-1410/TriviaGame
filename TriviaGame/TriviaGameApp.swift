2faaa8e1734cef3eb910c2e59fc45c123c827fbe
Jasleen-1410 <jasleen.minhas07@gmail.com>
2022-01-21 13:50:47 +0530

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Apple on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
